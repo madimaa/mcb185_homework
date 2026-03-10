9f1a9e72297556ba7d56ade8ba263fcbab917c09
madimaa <masondimaano@Masons-MacBook-Air-3.local>
2026-03-10 14:12:46 -0700

    new

--- class.py
+++ class.py
@@ -1,25 +1,42 @@
 import math
+import argparse
+import mcb185
+
+#entropy func
 def entropy(seq):
-	h = 0
 	pa = seq.count('A') / len(seq)
 	pc = seq.count('C') / len(seq)
 	pg = seq.count('G') / len(seq)
 	pt = seq.count('T') / len(seq)
+	h = 0
 	if pa != 0: h -= pa * math.log2(pa)
 	if pc != 0: h -= pc * math.log2(pc)
 	if pg != 0: h -= pg * math.log2(pg)
 	if pt != 0: h -= pt * math.log2(pt)
 	return h
-	
-seq = 'ACGTACGTACGTACGTAAAAAAACCCCCCCCGGGGGGGGGGTTTTTTT'
-# bunch of the same letters in a row = low entropy = masked by 'N'
-w = 5
-t = 1.5
-mask = list(seq)
-for i in range(len(seq) -w+1):
-	win = seq[i:i+w]
-	h = entropy(win)
-	if h < t:
-		for j in range(i, i+w):
-			mask[j] = 'N'
-print(''.join(mask))
+
+#argparse stuff is optional and just if you want to be professional
+parser = argparse.Argumentparser()
+parser.add_argument('fasta')
+parser.add_argument('--k', type=int, default=11)
+parser.add_argument('--threshold', type=float, default=1.1)
+parser.add_argument('--hard', help='perform hard')
+arg = parser.parse_args()
+
+seq = 'ACGTACGTAAAAAAAAAAACGTACGT'
+hard= 'ACGTACGTNNNNNNNNNNNCGTACGT'
+soft= 'ACGTACGTaaaaaaaaaaaCGTACGT'
+
+k = arg.window
+t = arg.threshold
+for defline, seq in mcb185.read_fasta(arg,fasta):
+#k = 5 # window size
+#t = 1.0 # entropy threshold
+	mask = list(seq)
+	for i in range(len(seq) -k+1):
+		if entropy(seq[i:i+k]) > t: continue
+		for j in range(i, i+k):
+			mask[j] = 'N' # hard
+			#mask[j] = seq[j].lower() # soft
+	print(''.join(mask))
+
--- study.py
+++ study.py
@@ -1,17 +1,18 @@
+import math
 import sys
-import random
 
-genome_size = int(sys.argv[1])
-coverage = int(sys.argv[2])
-
-genome = [0] * genome_size
-
-for i in range(genome_size * coverage):
-	pos = random.randint(0, genome_size-1)
-	genome[pos] += 1
-
-zeroes = 0
-for v in genome:
-	if v < 8: zeroes += 1
+def dkl(P, Q):
+	if not math.isclose(1.0, sum(P)): sys.exit('error')
+	if not math.isclose(1.0, sum(Q)): sys.exit('error')
+# make sure the histograms ^ sum to 1, that's why we math.isclose here
+	distance = 0
+	for p, q in zip(P, Q):
+		if p == 0: continue
+		if q == 0: continue
+		distance = p * math.log2(p/q)
+	return -distance
 	
-print(1-zeroes/genome_size)
+a = [0.25, 0.25, 0.25, 0.25]
+b = [0.4, 0.3, 0.2, 0.1]
+
+print(dkl(a, b))
